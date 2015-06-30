b164ade6c6bf13753a7f26574c51a0805d960543
jorge-carvalho <j-jorginho@hotmail.com>
2015-06-30 20:11:33 -0300

    Update make.bat

--- make.bat
+++ make.bat
@@ -1,5 +1,3 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\vcvarsall.bat"
-
 cl mandelbrot_sequencial.cpp
 cl mandelbrot_simd_avx.cpp
 cl mandelbrot_simd_mmx.cpp
@@ -8,6 +6,6 @@ for /f %%f in ('dir /b in\') do mandelbrot_sequencial in\%%f out\%%f
 for /f %%f in ('dir /b in\') do mandelbrot_simd_avx in\%%f out\SIMD_%%f
 for /f %%f in ('dir /b in\') do mandelbrot_simd_mmx in\%%f out\SIMD_%%f
 
-start mandelbrot_sequencial.exe
-start mandelbrot_simd_avx.exe
-start mandelbrot_simd_mmx.exe
+mandelbrot_sequencial
+mandelbrot_simd_avx
+mandelbrot_simd_mmx
